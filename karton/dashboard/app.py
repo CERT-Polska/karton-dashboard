ff3c315fe685cc88fadb2f367270d44bd4a040dc
Stanislav Rakovsky <iam@disasm.me>
2025-10-27 15:50:33 +0300

    QoL: formatted stacktrace

--- karton/dashboard/app.py
+++ karton/dashboard/app.py
@@ -105,6 +105,10 @@ class TaskView:
         return self._task.status
 
     @property
+    def error(self) -> TaskState:
+        return self._task.error
+
+    @property
     def last_update(self) -> datetime:
         return datetime.fromtimestamp(self._task.last_update)
 
